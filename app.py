3ad70e830b99a519af4f80b92e5e2bf17d504b43
vip-xman <xman_njupt@163.com>
2020-12-09 14:31:10 +0800

    cors

--- app.py
+++ app.py
@@ -12,16 +12,12 @@ from flask_cors import CORS, cross_origin
 from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
-config = {
-  'ORIGINS': [
-    'http://localhost:3000',
-    'http://127.0.0.1:5000',
-  ],
+CORS(app, resources={r'/*': {'origins': '*'}}, supports_credentials=True)
 
-  'SECRET_KEY': '...'
-}
-CORS(app, resources={r'/*': {'origins': config['ORIGINS']}}, supports_credentials=True)
-
+@app.after_request
+def creds(response):
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    return response
 
 #@app.route是一个装饰器，作用是：做一个url和视图函数的映射，URL和函数绑定，前端/浏览器访问某个url，就会调用这个函数。
 #创建主页路由
@@ -31,24 +27,29 @@ def index():
 
 '''
 login功能实现：将前端传过来的网址存进数据库，并开始爬取数据
-api接口前缀：apiPrefix = '/api/v1/'
-下面的语句，不用if、else，直接app.route(apiPrefix + '字符串',methods=['post']) api与前端连接传数据过来
 craw.test_sec改成craw.bili_spyder
 '''
 #默认视图函数，只能采用get请求；如果想采用post请求，要写明
 @app.route('/login/',methods=['GET','POST'])
+@cross_origin(origins='*')
 def login():
+    app.logger.info(request.method)
+    print('调用了这边的login呢')
+    #return 'hhhhhh'
     if request.method == 'GET':
         return render_template('login.html')
     else:
-        website = request.form.get('web')  #输入的网址
-        #cid = str(uuid.uuid1())  #查询区分的id
+        web_b = request.get_data()
+        web_str = web_b.decode('UTF-8')
+        website = web_str[12:-2]
+        print(website)
+        #website = request.form.get('web')  #输入的网址
         ct = time.localtime(time.time())
         cid = str(time.strftime("%Y-%m-%d %H:%M:%S", ct))
-        #print(cid)
         db_mongo.insert_websites(website, cid)  #存入数据库
         executor.submit(craw.bili_spyder, cid)  #craw.bili_spyder(cid)
-        return "正在爬取中，请稍等。5分钟后请打开网页'{}'，输入本次的查询时间：'{}'查询".format('127.0.0.1:5000/output/',cid)
+        return cid
+        #return "正在爬取中，请稍等。5分钟后请打开网页'{}'，输入本次的查询时间：'{}'查询".format('127.0.0.1:5000/output/',cid)
 
 
 @app.route('/output/',methods=['GET','POST'])
@@ -71,7 +72,7 @@ if __name__=='__main__':
     #app.run(port=5000, host='0.0.0.0')  # 没用WSGI，其他网络登陆不了
     server = WSGIServer(('0.0.0.0', 5000), app)
     server.serve_forever()
-    app.run()
+    app.run(debug=True)
 
 
 
